4ca304bac95e27bbe1fe561a69e211ce1b78eb1d
Aleksandar Drakaliyski <aleksandardrakaliyski@Mac.broadband>
2025-02-15 15:31:33 +0000

    finished project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,7 +11,21 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    let tipPercentages = [10, 15, 20, 25, 0]
+    @FocusState private var amountIsFocused: Bool
+    
+    @State private var selectedTip = 0
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(selectedTip)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal / peopleCount
+    }
+    
+    var totalAmount: Double {
+        return checkAmount + (checkAmount / 100 * Double(selectedTip))
+    }
     
     var body: some View {
         NavigationStack {
@@ -20,13 +34,38 @@ struct ContentView: View {
                     TextField("Amount", value: $checkAmount,
                               format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<101){ person in
+                            Text("\(person)")
+                        }
+                    }
+                }
+                    
+                Picker("Tip Percentage", selection: $selectedTip) {
+                        ForEach(0..<101, id: \.self){
+                            Text($0, format: .percent )
+                        }
+                    }
+                .pickerStyle(.navigationLink)
+                                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
                 
-                Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section("Total amount for the check") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle(Text("WeSplit"))
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
